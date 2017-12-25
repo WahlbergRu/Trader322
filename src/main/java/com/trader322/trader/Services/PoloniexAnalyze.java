2ca17f9f6b26ae98728879cb1a38e9ef056c4337
Nikita_Allin <nikita_allin@epam.com>
2017-12-25 17:58:24 +0300

    init

--- src/main/java/com/trader322/trader/Services/PoloniexAnalyze.java
+++ src/main/java/com/trader322/trader/Services/PoloniexAnalyze.java
@@ -3,12 +3,16 @@ package com.trader322.trader.Services;
 
 import com.trader322.trader.Model.PoloniexModel;
 import com.trader322.trader.Repository.PoloniexCurrencyRepository;
+import com.trader322.trader.Utils.Utils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.sql.Timestamp;
-import java.util.List;
+import java.util.*;
+import java.util.stream.Stream;
+
+import static com.trader322.trader.Utils.ConsoleColors.*;
 
 @Service("poloniexAnalyze")
 @Transactional
@@ -25,16 +29,102 @@ public class PoloniexAnalyze {
     public void checkCurrency(){
         Timestamp timestamp = new Timestamp(System.currentTimeMillis());
         List<PoloniexModel> poloniexModelList = poloniexCurrencyRepository.findByPoloniexIdentity_TimestampBetween(
-            timestamp.getTime() - 1800000, timestamp.getTime() - 1
+            timestamp.getTime() - 1800000, timestamp.getTime() - 0
         );
 
-//        poloniexModelList.stream().map(
-//            poloniexModel -> {
+        Map<String, List<PoloniexModel>> mapPoloniexModel= new HashMap<>();
+
+        poloniexModelList.forEach(
+            poloniexModel -> {
+                String poloniexModelName = poloniexModel.getName();
+                List<PoloniexModel> mapPoloniexModelName = mapPoloniexModel.get(poloniexModelName);
+                if (mapPoloniexModelName == null) {
+                    mapPoloniexModelName = new ArrayList<>();
+                    mapPoloniexModel.put(poloniexModelName,mapPoloniexModelName);
+                }
+                mapPoloniexModelName.add(poloniexModel);
+            }
+        );
+
+        System.out.println(poloniexModelList);
+
+        String discordResponse;
+
+        for (Map.Entry<String, List<PoloniexModel>> entry : mapPoloniexModel.entrySet()) {
+            Long currentTime = timestamp.getTime();
+            PoloniexModel lastPoloniex = entry.getValue().get(0);
+
+            entry.getValue().forEach(
+                (PoloniexModel poloniexModel) -> {
+
+                    if (Utils.timeBorders(poloniexModel.getPoloniexIdentity().getTimestamp(), currentTime, 60000)){
+                        System.out.println(WHITE + ("https://poloniex.com/exchange#"+poloniexModel.getName()));
+
+//                            logic of 1 minute
+                        System.out.println(
+                                YELLOW_BOLD + "1 minute: " +
+                                BLUE + poloniexModel.getName() + " " +
+                                YELLOW + (((lastPoloniex.getLast() - poloniexModel.getLast())/ lastPoloniex.getLast()) * 100) + GREEN + "%"
+                        );
+                    } else if (Utils.timeBorders(poloniexModel.getPoloniexIdentity().getTimestamp(), currentTime, 120000)) {
+                        System.out.println(
+                                YELLOW_BOLD + "2 minute: " +
+                                BLUE + poloniexModel.getName() + " " +
+                                YELLOW + (((lastPoloniex.getLast() - poloniexModel.getLast())/ lastPoloniex.getLast()) * 100) + GREEN + "%");
+                    } else if (Utils.timeBorders(poloniexModel.getPoloniexIdentity().getTimestamp(), currentTime, 300000)) {
+                        System.out.println(
+                                YELLOW_BOLD + "5 minute: " +
+                                BLUE + poloniexModel.getName() + " " +
+                                YELLOW + (((lastPoloniex.getLast() - poloniexModel.getLast())/ lastPoloniex.getLast()) * 100) + GREEN + "%");
+                    } else if (Utils.timeBorders(poloniexModel.getPoloniexIdentity().getTimestamp(), currentTime, 600000)) {
+                        System.out.println(
+                                YELLOW_BOLD + "10 minutes: " +
+                                        BLUE + poloniexModel.getName() + " " +
+                                        YELLOW + (((lastPoloniex.getLast() - poloniexModel.getLast())/ lastPoloniex.getLast()) * 100) + GREEN + "%");
+                    } else if (Utils.timeBorders(poloniexModel.getPoloniexIdentity().getTimestamp(), currentTime, 900000)) {
+                        System.out.println(
+                                YELLOW_BOLD + "15 minutes: " +
+                                        BLUE + poloniexModel.getName() + " " +
+                                        YELLOW + (((lastPoloniex.getLast() - poloniexModel.getLast())/ lastPoloniex.getLast()) * 100) + GREEN + "%");
+                    } else if (Utils.timeBorders(poloniexModel.getPoloniexIdentity().getTimestamp(), currentTime, 1200000)) {
+                        System.out.println(
+                                YELLOW_BOLD + "20 minutes: " +
+                                BLUE + poloniexModel.getName() + " " +
+                                YELLOW + (((lastPoloniex.getLast() - poloniexModel.getLast())/ lastPoloniex.getLast()) * 100) + GREEN + "%");
+                    } else if (Utils.timeBorders(poloniexModel.getPoloniexIdentity().getTimestamp(), currentTime, 1500000)) {
+                        System.out.println(
+                                YELLOW_BOLD + "25 minutes: " +
+                                        BLUE + poloniexModel.getName() + " " +
+                                        YELLOW + (((lastPoloniex.getLast() - poloniexModel.getLast())/ lastPoloniex.getLast()) * 100) + GREEN + "%");
+                    } else if (Utils.timeBorders(poloniexModel.getPoloniexIdentity().getTimestamp(), currentTime, 1800000)) {
+                        System.out.println(
+                                YELLOW_BOLD + "30 minutes: " +
+                                        BLUE + poloniexModel.getName() + " " +
+                                        YELLOW + (((lastPoloniex.getLast() - poloniexModel.getLast())/ lastPoloniex.getLast()) * 100) + GREEN + "%");
+                    } else {
+
+                    }
+                }
+            );
+
+//            System.out.println(entry.getValue());
+
+//            Stream<PoloniexModel> streamSortedListPoloniexModel = entry.getValue()
+//                    .stream()
+//                    .sorted(Comparator.comparingLong(f -> f.getPoloniexIdentity().getTimestamp()));
 //
-//            }
-//        );
+//            PoloniexModel poloniexModelFirst = streamSortedListPoloniexModel.findFirst().get();
 //
-//        System.out.println(poloniexModelList);
+//            streamSortedListPoloniexModel.filter(
+//                    poloniexModel -> {
+//                        (poloniexModel.getLast() - poloniexModelFirst.getLast())
+//                    }
+//                );
+//
+//            System.out.println("item " + entry.getKey() + " count : ");
+//
+//            System.out.println("Item : " + entry.getKey() + " Count : " + entry.getValue());
+        }
     }
 
 }
--- src/main/java/com/trader322/trader/Utils/Utils.java
+++ src/main/java/com/trader322/trader/Utils/Utils.java
@@ -1,7 +1,26 @@
 package com.trader322.trader.Utils;
 
+import static com.trader322.trader.Utils.ConsoleColors.*;
+
 public class Utils {
 
-    
+    public static boolean timeBorders (long timestamp, long currentTimestamp, long differentTimestamp){
+        long differentTime = timestamp - currentTimestamp;
+//        System.out.println(
+//                YELLOW + (differentTime < 0) + " " + (differentTime > -differentTimestamp) + " " +
+//                BLUE + differentTime + " " +
+//                CYAN + differentTimestamp + " " +
+//                RED + (differentTime + differentTimestamp) + " " +
+//                YELLOW + (differentTime > -differentTimestamp) + " " +
+//                YELLOW + (differentTime + differentTimestamp < 60000)
+//        );
+        boolean isExist = differentTime < 0 && differentTime > -differentTimestamp && differentTime + differentTimestamp < 60000;
+
+//        if (isExist){
+//            System.out.println(BLUE + timestamp + " " + RED + currentTimestamp + " " + CYAN + differentTime);
+//        }
+
+        return isExist;
+    }
 
 }
